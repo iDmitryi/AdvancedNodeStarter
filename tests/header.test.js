66bbb87e9ea7b65145d604bb366adc1c14a82f1d
iDmitryi <hdumitru1998@gmail.com>
2024-08-06 14:37:31 +0300

    add test to click login btn

--- tests/header.test.js
+++ tests/header.test.js
@@ -16,9 +16,18 @@ afterEach(async () => {
   await browser.close();
 });
 
-test("we can launch a browser", async () => {
+test("wthe header has the correct text", async () => {
   // #root > div > div > nav > div > a
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
   expect(text).toEqual("Blogster");
+});
+
+test("clicking login start oauth flow", async () => {
+  await page.click(".right a");
+
+  // https://accounts.google.com
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
