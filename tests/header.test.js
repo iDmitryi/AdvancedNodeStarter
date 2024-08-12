ae35d0fe805980b791429ab9dad7156b3c3b6ea8
DimaHng <dumitru@taveunipro.com>
2024-08-12 06:56:57 +0300

    add test for login

--- tests/header.test.js
+++ tests/header.test.js
@@ -3,9 +3,7 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-  });
+  browser = await puppeteer.launch({});
 
   page = await browser.newPage();
 
@@ -16,7 +14,7 @@ afterEach(async () => {
   await browser.close();
 });
 
-test("wthe header has the correct text", async () => {
+test("the header has the correct text", async () => {
   // #root > div > div > nav > div > a
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
@@ -30,4 +28,29 @@ test("clicking login start oauth flow", async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("when sign in, shows logout button", async () => {
+  const id = "66ae0316a6d51e92d6e4b386";
+
+  const Buffer = require("safe-buffer").Buffer;
+
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+
+  const keygrip = new Keygrip([keys.cookieKey]);
+
+  const sig = keygrip.sign("session=" + sessionString);
+
+  console.log(sessionString, sig);
 });
