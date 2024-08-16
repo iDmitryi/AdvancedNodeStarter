d4ccc21485ab3bcff9c976f906cd17d1802b7df2
DimaHng <dumitru@taveunipro.com>
2024-08-16 07:12:30 +0300

    add page helper

---
+++ tests/helpers/page.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox"],
+    });
+
+    const page = await browser.newPage();
+
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return customPage[property] || browser[property] || page[property];
+      },
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+}
+
+module.exports = CustomPage;
