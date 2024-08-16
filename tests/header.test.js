07a0c621f20db43f9ab87a8719938575f2cacd77
DimaHng <dumitru@taveunipro.com>
2024-08-16 07:28:16 +0300

    add helper function for $eval

--- tests/header.test.js
+++ tests/header.test.js
@@ -13,7 +13,7 @@ afterEach(async () => {
 
 test("the header has the correct text", async () => {
   // #root > div > div > nav > div > a
-  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  const text = await page.getContentsOf("a.brand-logo");
 
   expect(text).toEqual("Blogster");
 });
@@ -30,7 +30,7 @@ test("clicking login start oauth flow", async () => {
 test("when sign in, shows logout button", async () => {
   await page.login();
 
-  const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
 
   expect(text).toEqual("Logout");
 });
