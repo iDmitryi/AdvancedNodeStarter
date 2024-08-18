0abc04a341b9cdeacd7ac4727f531509434c05f5
DimaHng <dumitru@taveunipro.com>
2024-08-18 15:26:02 +0300

    tests when user is not logged in

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -62,3 +62,34 @@ describe("when logged in", async () => {
     });
   });
 });
+
+describe("user is not logged in", async () => {
+  test("user cannot create blog posts", async () => {
+    const result = await page.evaluate(() => {
+      return fetch("/api/blogs", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: "My title", content: "My content" }),
+      }).then((res) => res.json());
+    });
+
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+
+  test("user cannot get a list of posts", async () => {
+    const result = await page.evaluate(() => {
+      return fetch("/api/blogs", {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+    });
+
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+});
