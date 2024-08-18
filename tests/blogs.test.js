e52192bed8284fd8ce35a5b5000528aa466d8a5b
DimaHng <dumitru@taveunipro.com>
2024-08-18 14:36:25 +0300

    check blog form

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -4,19 +4,36 @@ let page;
 beforeEach(async () => {
   page = await Page.build();
 
-  await page.getContentsOf("http://localhost:3000");
+  await page.goto("http://localhost:3000");
 });
 
 afterEach(async () => {
   await page.close;
 });
 
-test("when logged in, can se blog create form", async () => {
-  await page.login();
+describe("when logged in", async () => {
+  beforeEach(async () => {
+    await page.login();
 
-  await page.click("a.btn-floating");
+    await page.click("a.btn-floating");
+  });
 
-  const label = page.getContentsOf("form label");
+  test("can se blog create form", async () => {
+    const label = page.getContentsOf("form label");
 
-  expect(label).toEqual("Blog Labels");
+    expect(label).toEqual("Blog Labels");
+  });
+
+  describe("and using invalid inputs", async () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+    test("the form shows an error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
+    });
+  });
 });
